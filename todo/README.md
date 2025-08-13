54febd2201aa98dd9f809db8a5c1ca43d8251a80
Anash <174944152+mehuanas@users.noreply.github.com>
2025-08-13 16:21:12 +0000

    Add files via upload

---
+++ todo/README.md
@@ -0,0 +1,19 @@
+# 📝 To-Do List App  
+
+A simple and responsive to-do list web app built with **HTML**, **CSS**, and **JavaScript**.  
+It allows you to add, edit, mark as completed, and delete tasks — with **localStorage** support so your tasks stay saved even after refreshing.  
+
+## ✨ Features  
+- ➕ **Add tasks** quickly  
+- ✏ **Edit existing tasks**  
+- ✅ **Mark tasks as completed or pending**  
+- ❌ **Delete tasks**  
+- 💾 **Data persistence** using localStorage  
+- 📱 **Responsive design** for all devices  
+
+## 📂 Technologies Used  
+- HTML5  
+- CSS3  
+- Vanilla JavaScript  
+- Bootstrap Icons (CDN)  
+- Google Fonts – *Inter*  
---
+++ todo/script.js
@@ -0,0 +1,123 @@
+const newTaskBtn = document.querySelector(".add-task-btn");
+const closeBtn = document.querySelector(".close-btn");
+const inputContainer = document.querySelector(".input-container");
+const taskInput = document.querySelector("#taskInput");
+const addBtn = document.querySelector("#addBtn");
+const tasksContainer = document.querySelector(".tasks");
+
+let tasks = JSON.parse(localStorage.getItem("taskData")) || [];
+let editingIndex = null;
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.shiftKey && e.key.toLocaleLowerCase() === "n") {
+    e.preventDefault();
+    inputContainer.classList.toggle("input-toggle");
+    addBtn.textContent = "Add";
+    taskInput.focus();
+  }
+});
+
+newTaskBtn.addEventListener("click", () => {
+  inputContainer.classList.toggle("input-toggle");
+  addBtn.textContent = "Add";
+  taskInput.focus();
+});
+closeBtn.addEventListener("click", () => {
+    inputContainer.classList.toggle("input-toggle");
+});
+
+function render() {
+  let clutter = tasks.map((task, index) => {
+    let status;
+    let icon;
+    task.completed ? (status = "completed") : (status = "pending");
+    task.completed
+      ? (icon = `<i data-index="${index}" class="bi bi-arrow-counterclockwise task-done"></i>`)
+      : (icon = `<i data-index="${index}" class="bi bi-check-circle-fill task-done"></i>`);
+    return `<div class="task">
+            <div class="text">
+              <p class="task-text ${status}">${task.text}</p>
+            </div>
+
+            <div class="task-actions">
+              ${icon}
+              <i data-index="${index}" class="bi bi-pencil-square edit-btn"></i>
+              <i data-index="${index}" class="bi bi-file-earmark-x-fill delete-btn"></i>
+            </div>
+          </div>`;
+  });
+
+  tasksContainer.innerHTML = clutter.join("");
+}
+
+render();
+
+function saveTask() {
+  localStorage.setItem("taskData", JSON.stringify(tasks));
+}
+
+taskInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    if (taskInput.value.trim() === "") {
+      alert("input box is empty");
+      return;
+    }
+    if (editingIndex === null) {
+      tasks.push({
+        text: taskInput.value.trim(),
+        completed: false,
+      });
+    } else {
+      tasks[editingIndex].text = taskInput.value.trim();
+      editingIndex = null;
+    }
+    saveTask();
+    render();
+    taskInput.value = "";
+    inputContainer.classList.toggle("input-toggle");
+  }
+});
+
+addBtn.addEventListener("click", () => {
+  if (taskInput.value.trim() === "") {
+    alert("input box is empty");
+    return;
+  }
+  if (editingIndex === null) {
+    tasks.push({
+      text: taskInput.value.trim(),
+      completed: false,
+    });
+  } else {
+    tasks[editingIndex].text = taskInput.value.trim();
+    saveTask();
+    editingIndex = null;
+  }
+  localStorage.setItem("taskData", JSON.stringify(tasks));
+  render();
+  taskInput.value = "";
+  inputContainer.classList.toggle("input-toggle");
+});
+
+tasksContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("task-done")) {
+    let index = e.target.dataset.index;
+    tasks[index].completed = tasks[index].completed ? false : true;
+    saveTask();
+    render();
+  }
+  if (e.target.classList.contains("edit-btn")) {
+    let index = e.target.dataset.index;
+    editingIndex = index;
+    inputContainer.classList.toggle("input-toggle");
+    taskInput.focus();
+    taskInput.value = tasks[index].text;
+    addBtn.textContent = "Update";
+  }
+  if (e.target.classList.contains("delete-btn")) {
+    let index = e.target.dataset.index;
+    tasks.splice(index, 1);
+    saveTask();
+    render();
+  }
+});
